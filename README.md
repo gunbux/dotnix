7b318a25727e4e7bac019c42ac40b0d5ddaecea5
Chun Yu <Chun Yu>
2024-04-08 00:58:45 +0800

    Added legion configs

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# Building the system
+## Initial Setup
+
+Ensure you are on the unstable channel:
+
+```bash
+nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+```
+
+Also add sane hardware defaults with nixos-hardware
+
+```bash
+sudo nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
+sudo nix-channel --update
+```
+
+
+## Building the system
 
 ```bash
 nixos-rebuild -I nixos-config=./base.nix switch
---
+++ hosts/legion/default.nix
@@ -0,0 +1,65 @@
+# TODO:
+# Switch to cherry picked zen kernel
+# asus-nb-wmi power consumption
+{config, pkgs, ... }:
+{
+  imports = [
+    <nixos-hardware/lenovo/legion/15arh05h>
+  ];
+
+  # Networking
+  networking.hostName = "legion-nix";
+  networking.networkmanager.wifi.powersave = true;
+
+  # Kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [
+    "initcall_blacklist=acpi_cpufreq_init"
+    "amd_pstate=active" # Enables amd_pstate_epp I believe?
+  ];
+
+  boot.kernelModules = [ "amd-pstate" ];
+
+  # NTFS Support
+  boot.supportedFilesystems = [ "ntfs" ];
+
+
+  # Power Mangement
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
+
+  # Auto CPU-Freq config
+  services.auto-cpufreq = {
+    enable = true;
+  };
+
+  # TLP Config, use one or the other but not both
+  # services.power-profiles-daemon.enable = false; # Defaults to true in Gnome 44
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_DRIVER_OPMODE_ON_AC="active";
+  #     CPU_DRIVER_OPMODE_ON_BAT="active";
+  #     CPU_SCALING_GOVERNOR_ON_AC="performance";
+  #     CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC="balance_performance";
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT="balance_power";
+  #     CPU_MIN_PERF_ON_AC=0;
+  #     CPU_MAX_PERF_ON_AC=100;
+  #     CPU_MIN_PERF_ON_BAT=0;
+  #     CPU_MAX_PERF_ON_BAT=30;
+  #   };
+  # };
+
+
+  # Services
+  services.upower.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+  ];
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = false;
+}
