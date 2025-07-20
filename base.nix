c2bb73b3ec29ec766614ee515ef19592a9ef03ad
Chun Yu <lamchunyu00@gmail.com>
2025-07-20 23:37:53 +0800

    feat: added garnix binary cache

--- base.nix
+++ base.nix
@@ -144,12 +144,14 @@
       keep-outputs = true;
       substituters = [
         "https://cache.nixos.org"
+        "https://cache.garnix.io"
         "https://nix-community.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
         "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
