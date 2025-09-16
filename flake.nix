8273f7b2f2c84b35d888afe46bf3923dc26656ba
Chun Yu <lamchunyu00@gmail.com>
2025-09-16 11:52:44 +0800

    fix: add backupFileExtension to g14 configs

--- flake.nix
+++ flake.nix
@@ -58,6 +58,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = {inherit inputs;};
+          home-manager.backupFileExtension = ".bak";
           home-manager.users.chun.imports = [
             inputs.nix-index-database.homeModules.nix-index
             ./home.nix
