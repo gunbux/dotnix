5f8f4a8f0671cb79244026e7e06ec26ed8ba7462
chun <lamchunyu00@gmail.com>
2025-10-20 14:29:19 +0800

    fix: remove marksman temporarily due to issues

--- config/lazyvim/lua/plugins/ale.lua
+++ config/lazyvim/lua/plugins/ale.lua
@@ -2,7 +2,7 @@ return {
   'dense-analysis/ale',
   config = function()
     vim.g.ale_fix_on_save = 1
-    vim.g.ale_linters = { zig = { 'zls' }, qml = { 'qmllint' }, rust = {}, go = {} }
+    vim.g.ale_linters = { zig = { 'zls' }, qml = { 'qmllint' }, rust = {}, go = {}, markdown = {} }
     vim.g.ale_fixers = { nix = { 'alejandra' }, zig = { 'zigfmt' }, qml = { 'qmlfmt' } }
   end
 }
