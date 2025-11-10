2ae53e58be82266d7a0c448006087759197cd9e6
Chun Yu <lamchunyu00@gmail.com>
2025-11-10 17:46:44 +0800

    refactor: removed unused configs

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -4,38 +4,6 @@
   lib,
   ...
 }: {
-  ## NOTE: These are old and kept for archival purposes only
-
-  # home.file.".config/hypr/monitors.conf" = lib.mkForce {
-  #   source = ../../config/hypr/monitors.conf;
-  #   force = true;
-  #   mutable = true;
-  # };
-
-  # home.file.".config/hypr/userprefs.conf" = lib.mkForce {
-  #   source = ../../config/hypr/userprefs.conf;
-  #   force = true;
-  #   mutable = true;
-  # };
-
-  # home.file.".config/hypr/hyde.conf" = lib.mkForce {
-  #   source = ../../config/hypr/hyde.conf;
-  #   force = true;
-  #   mutable = true;
-  # };
-
-  # home.file.".config/hypr/hypridle.conf" = lib.mkForce {
-  #   source = ../../config/hypr/hypridle.conf;
-  #   force = true;
-  #   mutable = true;
-  # };
-
-  # home.file.".config/waybar/config.ctl" = lib.mkForce {
-  #   source = ../../config/waybar/config.ctl;
-  #   force = true;
-  #   mutable = true;
-  # };
-
   home.file.".config/hypr/keybindings.conf" = lib.mkForce {
     source = ../../config/hypr/keybindings.conf;
     force = true;
