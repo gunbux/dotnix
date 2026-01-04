5deb4b8af5a02249dd265417d28081d3c5785f82
chun <lamchunyu00@gmail.com>
2026-01-04 13:53:01 +0800

    fix: disable calendar.vim

--- config/lazyvim/lua/plugins/calendar.lua
+++ config/lazyvim/lua/plugins/calendar.lua
@@ -1,5 +1,6 @@
 return {
   'itchyny/calendar.vim',
+  enabled = false,
   config = function()
     -- Enable Google Calendar integration
     vim.cmd('source ~/.cache/calendar.vim/credentials.vim')
@@ -7,6 +8,7 @@ return {
     vim.g.calendar_view = { 'year', 'month', 'week', 'day_4', 'agenda', 'clock' }
 
     -- Hotkey for opening Calendar in days view, with a vertical split on the right
-    vim.api.nvim_set_keymap('n', '<leader>c', ':Calendar -view=days -split=vertical -position=right<CR>', { noremap = true })
+    vim.api.nvim_set_keymap('n', '<leader>c', ':Calendar -view=days -split=vertical -position=right<CR>',
+      { noremap = true })
   end,
 }
